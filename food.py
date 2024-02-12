71d3e8433bd5aa5ca236987cb98d54113b1be3e5
RotaruSilviu <159648340+RotaruSilviu@users.noreply.github.com>
2024-02-12 20:44:10 +0200

    Update food.py

--- food.py
+++ food.py
@@ -1,10 +1,8 @@
 from turtle import Turtle
 import random
 
-
-### Turtle este superclass si incercam ca clasa food sa mosteneasca trasaturile ei.
 class Food(Turtle):
-    ### Clasa food a mostenit toate caracteristicile clasei Turtle si cream mancarea.
+
     def __init__(self):
         super().__init__()
         self.shape("circle")
@@ -12,7 +10,6 @@ class Food(Turtle):
         self.shapesize(stretch_len=0.5 , stretch_wid=0.5)
         self.color("red")
         self.speed(0)
-        ### Chemam functia refresh ca bucata de mancare odata cea fost atinsa sa se miste in alta parte.
         self.refresh()
 
     def refresh(self):
